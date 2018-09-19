ec39d36b56cf171a694a887ccf35f6230d37514d
xss5813 <xss5813@163.com>
2018-09-19 11:02:01 +0800

    #

---
+++ src/ios/Myplugin.h
@@ -0,0 +1,12 @@
+//
+//  SamAlertHelper.h
+//  房乐美微商城
+//
+//  Created by ykh007 on 2018/9/13.
+//
+
+
+#import <Cordova/CDVPlugin.h>
+@interface Myplugin : CDVPlugin
+-(void)showAlertWithTitle:(CDVInvokedUrlCommand *)command;
+@end
